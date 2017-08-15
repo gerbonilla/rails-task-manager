c4182f0e74ea4443cafcf0a99baaac3c40f869c9
Gerardo Bonilla <gerbonilla@gmail.com>
2017-08-15 12:57:12 +0200

    task manager completed - no design added

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.completed = false
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+
+end
+
+# Prefix Verb   URI Pattern               Controller#Action
+#     tasks GET    /tasks(.:format)          tasks#index
+#           POST   /tasks(.:format)          tasks#create
+#  new_task GET    /tasks/new(.:format)      tasks#new
+# edit_task GET    /tasks/:id/edit(.:format) tasks#edit
+#      task GET    /tasks/:id(.:format)      tasks#show
+#           PATCH  /tasks/:id(.:format)      tasks#update
+#           PUT    /tasks/:id(.:format)      tasks#update
+#           DELETE /tasks/:id(.:format)      tasks#destroy
